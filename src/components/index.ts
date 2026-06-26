d3d6dd60b5bade0705675aa0a36f234a6dd4b872
FKarchives <jeela2024@gmail.com>
2026-06-26 18:36:29 +0400

    Update components index to export CheckIn

---
+++ src/components/index.ts
@@ -0,0 +1,9 @@
+/**
+ * Component exports
+ * Centralized component library
+ */
+
+export { default as Button } from './Button'
+export { default as Card } from './Card'
+export { default as Input } from './Input'
+export { default as CheckIn } from './CheckIn'
